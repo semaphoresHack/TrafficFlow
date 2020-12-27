76feadc18e8be4ccee458b4cd6b4b817a7e5d8da
Shivang Tripathi <shivangtripathi2000@gmail.com>
2020-12-28 00:53:47 +0530

    feat: user app folder structure

---
+++ userApp/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const App = () => {
+    return (
+        <View>
+            <Text>Test it if it's working in your emulator</Text>
+        </View>
+    )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
