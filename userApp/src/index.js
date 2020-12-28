18abd0f61d1db56347a96a6f45cac370c7941a1f
Shivang Tripathi <shivangtripathi2000@gmail.com>
2020-12-28 22:44:07 +0530

    feat: first commit

--- userApp/src/index.js
+++ userApp/src/index.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import {PRIMARY_COLOR,RED} from './styles';
 
+//break your components into as small segments as you can
 const App = () => {
+    const [movies,setMovies] = useState([]);
+    const getMoviesFromApiAsync = async () => {
+        try {
+          let response = await fetch(
+            'https://reactnative.dev/movies.json'
+          );
+          let json = await response.json();
+          return json.movies;
+        } catch (error) {
+          console.error(error);
+        }
+      };
+    getMoviesFromApiAsync().then((data)=>{
+        setMovies(data);
+    });
     return (
-        <View>
-            <Text>Test it if it's working in your emulator</Text>
-        </View>
-    )
+        <>
+        <View style={{backgroundColor:PRIMARY_COLOR,flex:1,alignItems:'center',justifyContent:'space-around'}}>
+         {movies.map((item,index)=>{
+             return <Text key={index}>{item.title}</Text>
+         })}
+     </View>
+        </>
+        )
 }
 
 export default App
---
+++ userApp/src/styles/index.js
@@ -0,0 +1,7 @@
+export const PRIMARY_COLOR = "#1c4e6e";
+export const RED = "#bf312f";
+export const BLACK = "#000";
+export const GREEN = "#47ac4a";
+export const YELLOW = "#ffc53d";
+
+
