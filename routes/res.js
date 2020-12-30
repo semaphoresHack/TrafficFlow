aa2b16b9bd8eb8d25f84846e35c4d1b254773b15
Shikhar03Stark <hv.harshit321@gmail.com>
2020-12-31 01:37:09 +0530

    Backend API done

--- routes/res.js
+++ routes/res.js
@@ -1,12 +1,38 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const {createModel} = require('mongoose-gridfs');
 const router = express.Router();
 
+const dbUrl = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@trafficflow.sozdf.mongodb.net/trafficFlow?retryWrites=true&w=majority`;
+const conn = mongoose.createConnection(dbUrl, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+});
+
+let Resource;
+(async () => {
+    await conn.once('open', () => {
+        Resource = new mongoose.mongo.GridFSBucket(conn.db, {
+            bucketName : 'proofs',
+        })
+    });
+})();
+
+const binarydata = (filename, res) => {
+    try{
+        Resource.openDownloadStreamByName(filename).pipe(res);
+    }
+    catch (err) {
+        res.send(err);
+    }
+}
+
 //@meth : GET
-//route : /connectionStatus
-//remark : 
-router.get('/connectionStatus', (req, res) => {
-    const dbStatus = require('../server').connStatus;
-    res.send(`OK 200 : DB Status ${dbStatus}`);
+//route : /res/:id (ID is of filesystem file)
+//remark : this is media endpoint, response wil be binary
+router.get('/:name', (req, res) => {
+    const resourseName = req.params.name;
+    binarydata(resourseName, res);
 });
 
 module.exports = router;
